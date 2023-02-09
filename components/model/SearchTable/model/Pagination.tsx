4f5a03c21eaec60fe095b17a28ef02453a47b7bc
mozhaocong <>
2023-02-09 10:21:21 +0800

    更新

--- components/model/SearchTable/model/Pagination.tsx
+++ components/model/SearchTable/model/Pagination.tsx
@@ -9,31 +9,32 @@ export default (
 	}
 ) => {
 	if (!props.total) return ''
-	const styleData: any = { textAlign: 'right', padding: '8px 8px 0', borderTop: '1px solid #EEE', position: 'sticky', bottom: 0, background: '#FFF' }
 	return (
-		<div style={styleData}>
-			<Spin spinning={props.loading ?? false}>
-				<Pagination
-					size='small'
-					current={props.current || 1}
-					total={props.total || 50}
-					showTotal={total => `总条数 ${total}`}
-					pageSize={props.pageSize || 10}
-					showSizeChanger
-					showQuickJumper={true}
-					pageSizeOptions={[50, 100, 150]}
-					onChange={(current, pageSize) => {
-						if (props.onChange) {
-							props.onChange({ current, pageSize })
-						}
-					}}
-					onShowSizeChange={(current, pageSize) => {
-						if (props.onShowSizeChange) {
-							props.onShowSizeChange({ current, pageSize })
-						}
-					}}
-				/>
-			</Spin>
+		<div className={'table-pagination'}>
+			<div className={'table-pagination-div'}>
+				<Spin spinning={props.loading ?? false}>
+					<Pagination
+						size='small'
+						current={props.current || 1}
+						total={props.total || 50}
+						showTotal={total => `总条数 ${total}`}
+						pageSize={props.pageSize || 10}
+						showSizeChanger
+						showQuickJumper={true}
+						pageSizeOptions={[50, 100, 150]}
+						onChange={(current, pageSize) => {
+							if (props.onChange) {
+								props.onChange({ current, pageSize })
+							}
+						}}
+						onShowSizeChange={(current, pageSize) => {
+							if (props.onShowSizeChange) {
+								props.onShowSizeChange({ current, pageSize })
+							}
+						}}
+					/>
+				</Spin>
+			</div>
 		</div>
 	)
 }
